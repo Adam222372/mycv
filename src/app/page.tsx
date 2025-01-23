b13ad768183c7828ae579cf8457b33e796a95752
Adam222372 <adamholecek22@gmail.com>
2025-01-23 23:38:04 +0100

    Made hooks and dynamic "section" component, css visible/hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,14 @@
 "use client"
 import styles from "./page.module.css"
 import React from "react";
-
+import Section from "@/components/Section";
 export default function Home() {
-    const [clickedElement, setClickedElement] = React.useState<[string, boolean]>(["", false]);
-
-    const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-        const clickedDiv = event.currentTarget.id;
-        setClickedElement([clickedDiv, true]);
-    }
-    const handleCrossButtonClick = (): void => {
-        setClickedElement(["", false]);
-    };
-
     return (
         <div className={styles.gridContainer}>
-
-            <div
-                id="sectionA"
-                className={styles.sectionA}
-                onClick={handleClick}>
-                <div>
-                    <h1>About me</h1>
-                </div>
-                <div className={styles.profilePicture}/>
-                <button className={styles.crossButton} onClick={handleCrossButtonClick} style={{
-                    display: clickedElement[1] && clickedElement[0] === "sectionA" ? "block" : "none",
-                }}
-                >X</button>
-            </div>
-
-            <div
-                id="sectionB"
-                className={styles.sectionB}
-                onClick={handleClick}>
-                <h1>Carrier</h1>
-                <button className={styles.crossButton}>X</button>
-            </div>
-
-            <div id="sectionC" className={styles.sectionC} onClick={handleClick}>
-                <h1>Education</h1>
-                <button className={styles.crossButton}>X</button>
-            </div>
-
-            <div
-                id="sectionD"
-                className={styles.sectionD} onClick={handleClick}>
-                <h1>Experiences</h1>
-                <button className={styles.crossButton}>X</button>
-            </div>
+            <Section id={"sectionA"} title={"About me"} description={""}></Section>
+            <Section id={"sectionB"} title={"Carrier"} description={""}></Section>
+            <Section id={"sectionC"} title={"Education"} description={""}></Section>
+            <Section id={"sectionD"} title={"Experiences"} description={""}></Section>
         </div>
     );
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,28 +5,22 @@ interface Section {
     id: string,
     title: string,
     description: string,
-    image: string,
 }
 
-
-export default function Section({id, title, description, image}: Section) {
-    const [clicked, setClicked] = React.useState([id, false]);
-    const [hideBtn, setHideBtn] = React.useState(false);
+export default function Section({id, title, description}: Section) {
+    const [clicked, setClicked] = React.useState<[string, boolean]>([id, false]);
 
     const handleClick = () => {
         setClicked([id, true]);
     }
     const handleBtnHideClick = () => {
-
+        setClicked(["", false]);
     }
 
     return (
-        <div id={id} onClick={handleClick} className={}>
-            <div>
-                <h1>{title}</h1>
-                <h3>{description}</h3>
-            </div>
-            <div className={styles.profilePicture}/>
+        <div id={id} onClick={handleClick} className={clicked[0] === "" ? styles[id] : }>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
             <button className={styles.crossButton} onClick={handleBtnHideClick}>X</button>
         </div>
     )
