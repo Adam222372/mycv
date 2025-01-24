a0ea7d4ce89f0d847d8da6b8e57d0524473802fa
Adam222372 <adamholecek22@gmail.com>
2025-01-24 10:36:58 +0100

    removed transition

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -15,22 +15,19 @@ export default function Section({id, title, description}: Section) {
     const handleClick = () => {
         setClicked([id, true]);
     }
-    const handleBtnHideClick = () => {
+    /*const handleBtnHideClick = () => {
         setClicked(["", !clicked[1]]);
-    }
+    }*/
 
     return (
 
 
-        <motion.div onClick={handleClick} className={styles[id]} animate={{
-            gridColumn: clicked[1] ? '1 / -1' : styles.id,
-            gridRow: clicked[1] ? '1 / -1' : styles.id,
-        }} transition={{duration: 0.5}}>
+        <motion.div onClick={handleClick} className={styles[id]} style={{
+            gridColumn: clicked[1] ? '-1 / 1' : styles.id,
+            gridRow: clicked[1] ? '-1 / 1' : styles.id,
+        }}>
             <h1>{title}</h1>
             <h3>{description}</h3>
-            <button className={styles.crossButton} onClick={handleBtnHideClick}>X</button>
-
-
         </motion.div>
 
 
