2ec4d7f1693be9d8b6557b1b7a50b88a7313150c
Adam222372 <adamholecek22@gmail.com>
2025-02-01 22:42:47 +0100

    new config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: isProd ? "/your-repo-name" : "",
 };
 
 export default nextConfig;
