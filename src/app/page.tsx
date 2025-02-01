2585a133db6c1630c63ed4e8cce1382759709b76
Adam222372 <adamholecek22@gmail.com>
2025-02-01 21:20:48 +0100

    new useState() in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@ import styles from "./page.module.css"
 import React from "react";
 import Section from "@/components/Section";
 export default function Home() {
+    const [clickedIndex, setClickedIndex] = React.useState<string | undefined>(undefined);
+
+    function onClick(event: React.MouseEvent<HTMLButtonElement>) {
+        const id = event.currentTarget.id;
+        setClickedIndex(id);
+    }
+
     return (
         <div className={styles.gridContainer}>
-            <Section id={"sectionA"} title={"About me"} description={""}></Section>
-            <Section id={"sectionB"} title={"Carrier"} description={""}></Section>
-            <Section id={"sectionC"} title={"Education"} description={""}></Section>
-            <Section id={"sectionD"} title={"Experiences"} description={""}></Section>
+            <Section id={"sectionA"} title={"About me"} description={""} onClick={onClick}/>
+            <Section id={"sectionB"} title={"Carrier"} description={""} onClick={onClick}></Section>
+            <Section id={"sectionC"} title={"Education"} description={""} onClick={onClick}></Section>
+            <Section id={"sectionD"} title={"Experiences"} description={""} onClick={onClick}></Section>
         </div>
     );
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,34 +1,21 @@
 import styles from "@/app/page.module.css";
 import React from "react";
-import {motion} from "motion/react"
 
 
 interface Section {
     id: string,
     title: string,
     description: string,
+    onClick: (event: React.MouseEvent) => void,
 }
 
-export default function Section({id, title, description}: Section) {
-    const [clicked, setClicked] = React.useState<[string, boolean]>([id, false]);
-
-    const handleClick = () => {
-        setClicked([id, true]);
-    }
-    /*const handleBtnHideClick = () => {
-        setClicked(["", !clicked[1]]);
-    }*/
+export default function Section({id, title, description, onClick}: Section) {
 
     return (
-
-
-        <motion.div onClick={handleClick} className={styles[id]} style={{
-            gridColumn: clicked[1] ? '-1 / 1' : styles.id,
-            gridRow: clicked[1] ? '-1 / 1' : styles.id,
-        }}>
+        <div onClick={onClick} className={styles[id]}>
             <h1>{title}</h1>
             <h3>{description}</h3>
-        </motion.div>
+        </div>
 
 
     )
